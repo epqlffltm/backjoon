107c1cb3603ad21a84ff452701c6bee0ddd53371
epqlffltm <dsfake1225@gmail.com>
2025-05-23 08:38:59 +0900

    노트북 세 대를 가지고 왔다

--- bronze5/32775.py
+++ bronze5/32775.py
@@ -1,4 +1,2 @@
-S = int(input())
-F = int(input())
-
-print("high speed rail" if S <= F else "flight")
+T1, T2 = map(int, input().split())
+print(min(T1, T2))
---
+++ bronze5/33515.py
@@ -0,0 +1,5 @@
+#노트북 세 대를 가지고 왔다
+T1=int(input())
+T2=int(input())
+
+print(T1 if T1<T2 else T2)
