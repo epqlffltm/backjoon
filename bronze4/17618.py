4cd461526fc440e574288820b6b5444e98d5d647
epqlffltm <dsfake1225@gmail.com>
2025-05-28 21:48:27 +0900

    176181번 78점 받음...

---
+++ bronze4/17618.py
@@ -0,0 +1,14 @@
+def is_harshad(x):
+    s = sum(map(int, str(x)))
+    return x % s == 0
+
+def count_harshad(N):
+    cnt = 0
+    for i in range(1, N + 1):
+        if is_harshad(i):
+            cnt += 1
+    return cnt
+
+if __name__ == "__main__":
+    N = int(input())
+    print(count_harshad(N))
